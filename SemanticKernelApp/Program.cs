671dee7cb878cacf0ef0b666bde7b2d8fa821b55
Florian Wagner <h2floh@h2floh.net>
2024-08-21 13:52:09 +0000

    add keyvault secrets

--- SemanticKernelApp/Program.cs
+++ SemanticKernelApp/Program.cs
@@ -1,9 +1,18 @@
+using Azure.Identity;
+using Microsoft.SemanticKernel;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add Azure Key Vault to the configuration
+var keyVaultName = builder.Configuration["KeyVaultName"];
+var keyVaultUri = new Uri($"https://{keyVaultName}.vault.azure.net/");
+
+builder.Configuration.AddAzureKeyVault(keyVaultUri, new DefaultAzureCredential());
 
 var app = builder.Build();
 

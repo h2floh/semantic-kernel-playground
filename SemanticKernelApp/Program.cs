b9db6af6294258773e71828b3bcc96891091ab41
Florian Wagner <h2floh@h2floh.net>
2024-08-21 15:20:01 +0000

    integrate Azure openAI

--- SemanticKernelApp/Program.cs
+++ SemanticKernelApp/Program.cs
@@ -1,5 +1,8 @@
 using Azure.Identity;
 using Microsoft.SemanticKernel;
+using Microsoft.SemanticKernel.ChatCompletion;
+using Microsoft.SemanticKernel.Connectors.OpenAI;
+using System.Text.Json;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -23,14 +26,42 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-// 
-
 app.UseHttpsRedirection();
 
-app.MapPost("/message", (Message message) =>
+// Init semantic Kernel
+// Create a kernel with Azure OpenAI chat completion
+var semanticBuilder = Kernel.CreateBuilder().AddAzureOpenAIChatCompletion(app.Configuration["openAiDeployment"]!, app.Configuration["openAiEndpoint"]!, app.Configuration["openaikey"]!);
+
+// Add enterprise components
+semanticBuilder.Services.AddLogging(services => services.AddConsole().SetMinimumLevel(LogLevel.Trace));
+
+// Build the kernel
+Kernel kernel = semanticBuilder.Build();
+var chatCompletionService = kernel.GetRequiredService<IChatCompletionService>();
+
+// Enable planning
+OpenAIPromptExecutionSettings openAIPromptExecutionSettings = new() 
+{
+    ToolCallBehavior = ToolCallBehavior.AutoInvokeKernelFunctions
+};
+
+// Create a history store the conversation
+var history = new ChatHistory();
+
+app.MapPost("/message", async (Message message) =>
 {
     Console.WriteLine($"Message received: {message.message}");
-    return message.message;
+    // Add user input
+    history.AddUserMessage(message.message);
+    // Get the response from the AI
+    var result = await chatCompletionService.GetChatMessageContentAsync(
+        history,
+        executionSettings: openAIPromptExecutionSettings,
+        kernel: kernel);
+    // Add the message from the agent to the chat history
+    history.AddMessage(result.Role, result.Content ?? string.Empty);
+
+    return JsonSerializer.Serialize<Message>(new Message(result.Content ?? string.Empty));
 })
 .WithName("PostMessage")
 .WithOpenApi();
